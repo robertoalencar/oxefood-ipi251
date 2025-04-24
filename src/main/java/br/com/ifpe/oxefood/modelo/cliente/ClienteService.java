77abbdd20343e989f8100b6aa1bf305a10fd4a9c
Roberto Alencar <rroberto.alencar@gmail.com>
2025-04-23 21:31:30 -0300

    enviando arquivos da aula

--- src/main/java/br/com/ifpe/oxefood/modelo/cliente/ClienteService.java
+++ src/main/java/br/com/ifpe/oxefood/modelo/cliente/ClienteService.java
@@ -1,5 +1,7 @@
 package br.com.ifpe.oxefood.modelo.cliente;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -18,6 +20,16 @@ public class ClienteService {
         Cliente c  = repository.save(cliente);
 
         return c;
+    }
+
+    public List<Cliente> listarTodos() {
+  
+        return repository.findAll(); //SELECT * FROM Cliente
+    }
+
+    public Cliente obterPorID(Long id) {
+
+        return repository.findById(id).get(); //SELECT * FROM Cliente WHERE id = ?
     }
 
 }
